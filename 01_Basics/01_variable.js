5d26908ad349ce05d9576761481a4f4ca153a46c
Satyajeetdesai <165277832+Satyajeetdesai@users.noreply.github.com>
2024-03-28 12:24:10 +0000

    container

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountemail = "satya@gmail.com"
+var accountPassword = "12345"
+accountcity ="Mapusa"
+
+// accountId= 2
+
+
+accountemail="hc.com"
+accountPassword="20000"
+accountcity="panjim"
+
+console.table([accountId ,accountPassword , accountemail, accountcity])
