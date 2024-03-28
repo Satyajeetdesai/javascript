d6b11139b62f4dfac6c7de0a47bf0cfa9dc79161
Satyajeetdesai <165277832+Satyajeetdesai@users.noreply.github.com>
2024-03-28 17:20:00 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,9 @@
+let score ="33"
+//console.log(typeof score);
+
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+let islogged = hr
+let boolean = Boolean(islogged)
+console.log(boolean);
